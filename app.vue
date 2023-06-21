35740134b1dcfc1bded084d09b4c3950afb17aea
corey <csmaller@elumatherapy.com>
2023-06-21 16:35:30 -0600

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h5>hello</h5>
+    <NuxtPage />
+  </div>
+</template>
+<style lang="scss">
+@use '~/style/main.scss';
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@use "@/style/_colors.scss" as *;'
+          }
+        }
+      }
+    }
+})
+
