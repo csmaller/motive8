296fc78c5eabbeaf52fdd58b3b922b0d836a8370
corey <csmaller@elumatherapy.com>
2023-07-11 11:19:51 -0600

    add FA again

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <script setup>
 useHead({
   script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+  link: [
+    { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' },
+  ],
 });
 
 const colorMode = useColorMode();
