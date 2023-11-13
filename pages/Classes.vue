4dc6cff0e6490d266177d66e489fd0afb6efd5c4
corey <csmaller@elumatherapy.com>
2023-11-13 14:30:02 -0700

    classes

---
+++ pages/Classes.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { data: classesQuery } = await useAsyncData('classes', () => {
+  return queryContent('/classes').where({ title: 'classes' }).findOne();
+});
+</script>
+
+<template>
+  <div>
+    <h2 v-if="classesQuery" class="classes w-full p-3 mb-4 text-center">
+      <ContentRendererMarkdown :value="classesQuery" />
+    </h2>
+  </div>
+</template>
+<style scoped lang="scss">
+.classes {
+  font-size: 20px;
+}
+</style>
