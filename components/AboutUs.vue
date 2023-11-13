cd845ab95bf41cc73530fb5c1e9bed56302b4ebc
corey <csmaller@elumatherapy.com>
2023-11-12 20:19:51 -0700

    config

---
+++ components/AboutUs.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { data: aboutQuery } = await useAsyncData('about', () => {
+  return queryContent('/about').find();
+});
+</script>
+
+<template>
+  <div>
+    <h2 v-if="aboutQuery" class="about w-full p-3 mb-4 text-center">
+      <ContentRendererMarkdown :value="aboutQuery" />
+    </h2>
+  </div>
+</template>
+<style scoped lang="scss">
+.about {
+  font-size: 20px;
+}
+</style>
--- pages/About.vue
+++ pages/About.vue
@@ -1,14 +1,8 @@
-<script setup lang="ts">
-const { data: aboutQuery } = await useAsyncData('about', () => {
-  return queryContent('/about').find();
-});
-</script>
-
 <template>
   <div>
-    <pre>{{ aboutQuery }}</pre>
-    <div v-if="aboutQuery" class="about w-full p-3 mb-4 text-center">
+    <Mission />
+    <!-- <div v-if="aboutQuery" class="about w-full p-3 mb-4 text-center">
       <ContentRendererMarkdown :value="aboutQuery" />
-    </div>
+    </div> -->
   </div>
 </template>
