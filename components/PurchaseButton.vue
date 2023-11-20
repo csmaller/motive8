31c4a4c520d481fcbdd608e9326ecdf9ef7aaea8
corey <csmaller@elumatherapy.com>
2023-11-19 18:34:45 -0700

    fix link

---
+++ components/PurchaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    class="flex flex-column velocity"
+    href="https://checkout.stripe.com/c/pay/cs_live_a1RiFBIhP5mlmWjX2OdlsfzymyadWCMSyOSPI4mALBQpyLfLvI1euGA2NC#fidkdWxOYHwnPyd1blppbHNgWjA0SjF1Y0JGT3UzdX1OUE9jYEhuNzxtM3U1UEZvcjNTbWNGVDBtR1FHZkRkSE1OZDxiRGRBRFQ8a0sxM2h1MVZAdmNkd21JMmRXf1R3UWJrbkkxNU5sVH1yNTVNVWlsbkpEZicpJ3VpbGtuQH11anZgYUxhJz8nPXJIZktfZlRpZlVhPW9GZEBCJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmR1dStzdHNgaWpmbHF8K2ZqaConeCUl"
+    targe="_blank"
+  >
+    <img src="/img/velocity-logo.jpeg" />
+    <Button link label="Click to Join" class="btn p-button font-bold px-6 py-3 my-0 mb-5" />
+  </a>
+</template>
+<style lang="scss" scoped>
+.velocity {
+  width: 200px;
+  img,
+  button {
+    max-width: 150px;
+  }
+}
+</style>
--- pages/Classes.vue
+++ pages/Classes.vue
@@ -7,17 +7,17 @@ const { data: classesQuery } = await useAsyncData('classes', () => {
 <template>
   <div class="w-full justify-content-center">
     <div class="header text-center border-none">
-      <Button
-        link
-        label="Click to Join"
-        class="btn p-button font-bold px-6 py-3 w-4 my-3 mb-5"
+      <a
+        class="btn p-button font-bold px-6 py-3 w-4 my-3 mb-5 no-underline text-center"
         target="_blank"
         href="https://checkout.stripe.com/c/pay/cs_live_a1RiFBIhP5mlmWjX2OdlsfzymyadWCMSyOSPI4mALBQpyLfLvI1euGA2NC#fidkdWxOYHwnPyd1blppbHNgWjA0SjF1Y0JGT3UzdX1OUE9jYEhuNzxtM3U1UEZvcjNTbWNGVDBtR1FHZkRkSE1OZDxiRGRBRFQ8a0sxM2h1MVZAdmNkd21JMmRXf1R3UWJrbkkxNU5sVH1yNTVNVWlsbkpEZicpJ3VpbGtuQH11anZgYUxhJz8nPXJIZktfZlRpZlVhPW9GZEBCJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmR1dStzdHNgaWpmbHF8K2ZqaConeCUl"
-      />
+      >
+        Click to Join
+      </a>
       <h2 class="mt-5 underline">Class Times</h2>
     </div>
 
-    <div v-if="classesQuery" class="classes w-full p-3 mb-4 text-center">
+    <div v-if="classesQuery" class="classes w-full p-3 mb-4 justify-content-center">
       <ContentRendererMarkdown :value="classesQuery" />
     </div>
   </div>
