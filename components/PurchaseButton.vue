8f47570004637df91719f350b363b4a95ba859ff
corey <csmaller@elumatherapy.com>
2023-11-19 18:50:06 -0700

    update button

--- components/PurchaseButton.vue
+++ components/PurchaseButton.vue
@@ -1,19 +1,23 @@
 <template>
   <a
-    class="flex flex-column velocity"
+    class="flex velocity no-underline"
     href="https://checkout.stripe.com/c/pay/cs_live_a1RiFBIhP5mlmWjX2OdlsfzymyadWCMSyOSPI4mALBQpyLfLvI1euGA2NC#fidkdWxOYHwnPyd1blppbHNgWjA0SjF1Y0JGT3UzdX1OUE9jYEhuNzxtM3U1UEZvcjNTbWNGVDBtR1FHZkRkSE1OZDxiRGRBRFQ8a0sxM2h1MVZAdmNkd21JMmRXf1R3UWJrbkkxNU5sVH1yNTVNVWlsbkpEZicpJ3VpbGtuQH11anZgYUxhJz8nPXJIZktfZlRpZlVhPW9GZEBCJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmR1dStzdHNgaWpmbHF8K2ZqaConeCUl"
     targe="_blank"
   >
     <img src="/img/velocity-logo.jpeg" />
-    <Button link label="Click to Join" class="btn p-button font-bold px-6 py-3 my-0 mb-5" />
+    <div class="btn p-button font-bold px-6 py-3 my-0 mb-5 border-noround-left">Click To Join</div>
   </a>
 </template>
 <style lang="scss" scoped>
 .velocity {
-  width: 200px;
+  width: 300px;
   img,
-  button {
-    max-width: 150px;
+  a {
+    max-width: 300px;
+  }
+  img,
+  a {
+    height: 60px;
   }
 }
 </style>
--- pages/Classes.vue
+++ pages/Classes.vue
@@ -6,18 +6,12 @@ const { data: classesQuery } = await useAsyncData('classes', () => {
 
 <template>
   <div class="w-full justify-content-center">
-    <div class="header text-center border-none">
-      <a
-        class="btn p-button font-bold px-6 py-3 w-4 my-3 mb-5 no-underline text-center"
-        target="_blank"
-        href="https://checkout.stripe.com/c/pay/cs_live_a1RiFBIhP5mlmWjX2OdlsfzymyadWCMSyOSPI4mALBQpyLfLvI1euGA2NC#fidkdWxOYHwnPyd1blppbHNgWjA0SjF1Y0JGT3UzdX1OUE9jYEhuNzxtM3U1UEZvcjNTbWNGVDBtR1FHZkRkSE1OZDxiRGRBRFQ8a0sxM2h1MVZAdmNkd21JMmRXf1R3UWJrbkkxNU5sVH1yNTVNVWlsbkpEZicpJ3VpbGtuQH11anZgYUxhJz8nPXJIZktfZlRpZlVhPW9GZEBCJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmR1dStzdHNgaWpmbHF8K2ZqaConeCUl"
-      >
-        Click to Join
-      </a>
+    <div class="header text-center border-none justify-content-center">
+      <div class="w-full justify-content-center flex"><PurchaseButton /></div>
       <h2 class="mt-5 underline">Class Times</h2>
     </div>
 
-    <div v-if="classesQuery" class="classes w-full p-3 mb-4 justify-content-center">
+    <div v-if="classesQuery" class="classes w-full p-3 mb-4 justify-content-center text-center">
       <ContentRendererMarkdown :value="classesQuery" />
     </div>
   </div>
