85f8074a761c1eee8525baf551f12fc1e8612a27
corey <csmaller@elumatherapy.com>
2023-11-19 18:57:50 -0700

    links

--- components/PurchaseButton.vue
+++ components/PurchaseButton.vue
@@ -1,9 +1,8 @@
+<script lang="ts" setup>
+const url = ref('https://buy.stripe.com/8wMcNZcQlcPd8jCaEG');
+</script>
 <template>
-  <a
-    class="flex velocity no-underline"
-    href="https://checkout.stripe.com/c/pay/cs_live_a1RiFBIhP5mlmWjX2OdlsfzymyadWCMSyOSPI4mALBQpyLfLvI1euGA2NC#fidkdWxOYHwnPyd1blppbHNgWjA0SjF1Y0JGT3UzdX1OUE9jYEhuNzxtM3U1UEZvcjNTbWNGVDBtR1FHZkRkSE1OZDxiRGRBRFQ8a0sxM2h1MVZAdmNkd21JMmRXf1R3UWJrbkkxNU5sVH1yNTVNVWlsbkpEZicpJ3VpbGtuQH11anZgYUxhJz8nPXJIZktfZlRpZlVhPW9GZEBCJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmR1dStzdHNgaWpmbHF8K2ZqaConeCUl"
-    targe="_blank"
-  >
+  <a class="flex velocity no-underline" :href="url" targe="_blank">
     <img src="/img/velocity-logo.jpeg" />
     <div class="btn p-button font-bold px-6 py-3 my-0 mb-5 border-noround-left">Click To Join</div>
   </a>
