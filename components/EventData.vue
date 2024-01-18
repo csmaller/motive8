d3355291bd1f3183207c17d3a4b61fcbf9289b31
corey <csmaller@elumatherapy.com>
2024-01-18 16:58:39 -0700

    image3 class for events

--- components/EventData.vue
+++ components/EventData.vue
@@ -6,8 +6,8 @@ const { data: eventQuery } = await useAsyncData('events', () => {
 
 <template>
   <div v-if="eventQuery" class="news w-full p-3 mb-4 font-bold text-center">
-    <div class="img-container flex flex-wrap w-full justify-content-center align-content-end">
-      <img v-if="eventQuery.image" :alt="`${eventQuery.image}`" :src="eventQuery.image" />
+    <div class="img-container flex flex-wrap w-full justify-content-center align-content-end w-full">
+      <img v-if="eventQuery.image" :alt="`${eventQuery.image}`" :src="eventQuery.image" :class="eventQuery.class" />
     </div>
     <ContentRendererMarkdown :value="eventQuery" />
   </div>
