5ec2d8c764fb879b657d07beb72500d0d6f8dda0
corey <csmaller@elumatherapy.com>
2024-11-11 20:58:46 -0700

    coaches

--- components/Coaches.vue
+++ components/Coaches.vue
@@ -5,8 +5,11 @@ const { data: coachesQuery } = await useAsyncData('coaches', () => {
 </script>
 
 <template>
-  <LayoutContainer class-name="flex-column sm:flex-column w-auto sm:w-full md:flex-wrap px-5 sm:px-1 md:px-5">
-    <div v-if="coachesQuery" class="coaches-container p-0 mb-5 justify-content-between lg:gap-5">
+  <LayoutContainer class-name=" w-auto sm:w-full  px-5 sm:px-1 md:px-5">
+    <div
+      v-if="coachesQuery"
+      class="coaches-container flex flex-column sm:flex-column lg:flex-row p-0 mb-5 justify-content-between lg:gap-5"
+    >
       <Profile v-for="person in coachesQuery" :person="person" :key="person.id" />
     </div>
   </LayoutContainer>
