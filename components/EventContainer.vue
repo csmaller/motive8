9693f8ef30a2dc81bed800f4d95b3c4c779dba46
corey <csmaller@elumatherapy.com>
2025-04-03 17:57:20 -0600

    test

--- components/EventContainer.vue
+++ components/EventContainer.vue
@@ -25,35 +25,44 @@ const isInDateRange = (start: string, end: string) => {
 </script>
 
 <template>
-  <div
-    class="event flex align-content-center p-0 justify-content-between gap-2 my-5 w-full"
-    :class="expand ? 'expand' : null"
-    v-bind:style="{ backgroundColor }"
-  >
-    <div class="flex grid w-full">
-      <div class="flex w-full">
-        <img :src="event.image" :alt="event.description" class="w-full" />
-      </div>
-      <div
-        class="content flex flex-column align-content-center w-full justify-content-center col-12 sm:col-12 lg:col-6 py-4 gap-2"
-      >
-        <h2 class="mb-2">{{ event.title }}</h2>
-        <h5>{{ event.description }}</h5>
-        <small>Start Of Event: {{ prettyDate(event.event_date_start) }}</small>
-        <small>End Of Event: {{ prettyDate(event.event_date_end) }}</small>
-        <small>Time Start: {{ prettyTime(event.event_date_start) }}</small>
-      </div>
-      <div class="payment flex flex-column col-12 sm:col-12 lg:col-3 align-items-center gap-3 my-6">
-        <PurchaseButton v-if="event && event.link" :link="event.link" name="Sign Up" />
-      </div>
-      <div class="content flex align-content-center justify-content-center col-12 p-4 border-1">
-        <div class="main-content flex flex-column">
-          <h2 class="itinerary w-full text-center hidden sm:block lg:hidden mb-3">ITINERARY</h2>
-          <ContentRendererMarkdown :value="event" />
-          <div class="payment flex flex-column align-items-center gap-3 my-6">
-            <PurchaseButton v-if="event && event.link" :link="event.link" name="Sign Up" />
+  <div>
+    <div
+      class="event flex align-content-center p-0 justify-content-between gap-2 my-5 w-full"
+      :class="expand ? 'expand' : null"
+      v-bind:style="{ backgroundColor }"
+      v-if="event.pdf === '' || event.pdf === undefined"
+    >
+      <div class="flex grid w-full">
+        <div class="flex w-full">
+          <img :src="event.image" :alt="event.description" class="w-full" />
+        </div>
+        <div
+          class="content flex flex-column align-content-center w-full justify-content-center col-12 sm:col-12 lg:col-6 py-4 gap-2"
+        >
+          <h2 class="mb-2">{{ event.title }}</h2>
+          <h5>{{ event.description }}</h5>
+          <small>Start Of Event: {{ prettyDate(event.event_date_start) }}</small>
+          <small>End Of Event: {{ prettyDate(event.event_date_end) }}</small>
+          <small>Time Start: {{ prettyTime(event.event_date_start) }}</small>
+        </div>
+        <div class="payment flex flex-column col-12 sm:col-12 lg:col-3 align-items-center gap-3 my-6">
+          <PurchaseButton v-if="event && event.link" :link="event.link" name="Sign Up" />
+        </div>
+        <div class="content flex align-content-center justify-content-center col-12 p-4 border-1">
+          <div class="main-content flex flex-column">
+            <h2 class="itinerary w-full text-center hidden sm:block lg:hidden mb-3">ITINERARY</h2>
+            <ContentRendererMarkdown :value="event" />
+            <div class="payment flex flex-column align-items-center gap-3 my-6">
+              <PurchaseButton v-if="event && event.link" :link="event.link" name="Sign Up" />
+            </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div v-else class="event flex align-content-start p-0 justify-content-center w-full">
+      <ContentRendererMarkdown :value="event" />
+      <div class="payment flex flex-column align-items-center gap-3 my-6">
+        <PurchaseButton v-if="event && event.link" :link="event.link" name="Sign Up" />
       </div>
     </div>
   </div>
